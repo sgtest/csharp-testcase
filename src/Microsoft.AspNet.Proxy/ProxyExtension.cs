69b949cc974cb1a2f3af0231d7a533437fe9fc0b
Ildar Isaev <iisaev@codeminders.com>
2016-02-29 09:45:07 -0800

    testdata simplification

--- src/Microsoft.AspNet.Proxy/ProxyExtension.cs
+++ src/Microsoft.AspNet.Proxy/ProxyExtension.cs
@@ -5,17 +5,6 @@ using Microsoft.AspNet.Proxy;
 
 namespace Microsoft.AspNet.Builder
 {
-    public static class ProxyExtension
-    {
-        /// <summary>
-        /// Sends request to remote server as specified in options
-        /// </summary>
-        /// <param name="builder"></param>
-        /// <param name="options">Options for setting port, host, and scheme</param>
-        /// <returns></returns>
-        public static IApplicationBuilder RunProxy(this IApplicationBuilder builder, ProxyOptions options)
-        {
-            return builder.UseMiddleware<ProxyMiddleware>(options);
-        }
-    }
+    public static class D
+    { }
 }
